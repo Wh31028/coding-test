0d1cf1f7bc21ea8fa89a48ae9f6e752876f1ff01
Wh31028 <wh31028@naver.com>
2025-02-16 14:09:15 +0900

    boj 11726 dp

---
+++ boj/B11726/src/Main.java
@@ -0,0 +1,22 @@
+import java.io.*;
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		br.close();
+		
+		long[] result = new long[1001];
+		
+		result[1] = 1;
+		result[2] = 2;
+		
+		for(int i=3;i<=n;i++) {
+			result[i] = (result[i-1]+result[i-2])%10007;
+			
+		}
+		System.out.print(result[n]);
+
+	}
+
+}
