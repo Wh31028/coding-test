e180dabebc849e505fba9db4a318dbbde0b05422
Wh31028 <wh31028@naver.com>
2025-03-24 11:15:25 +0900

    boj 10799 스택

---
+++ boj/자료구조/B10799/src/Main.java
@@ -0,0 +1,28 @@
+import java.io.*;
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String test = String.valueOf(br.readLine());
+		
+		
+		int count = 1;
+		int ans = 0;
+		for(int i=1;i<test.length();i++) {
+			
+			if(test.charAt(i)==')'&&test.charAt(i-1)=='(') {
+				count--;
+				ans += count;
+			}else if(test.charAt(i)=='(') {
+				count++;
+			}else if(test.charAt(i)==')') {
+				count--;
+				ans++;
+			}
+		}
+		System.out.print(ans);
+
+	}
+
+}
