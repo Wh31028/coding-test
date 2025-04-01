772363df0fb6ae076761ad4b4c0a2a99e3a309e7
Wh31028 <wh31028@naver.com>
2025-04-01 12:32:42 +0900

    boj 10430 구현

---
+++ boj/구현,수학,문자열/B10430/src/Main.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		
+		bw.write(String.valueOf((A+B)%C));
+		bw.newLine();
+		bw.write(String.valueOf(((A%C)+(B%C))%C));
+		bw.newLine();
+		bw.write(String.valueOf((A*B)%C));
+		bw.newLine();
+		bw.write(String.valueOf((A%C)*(B%C)%C));
+		
+		bw.flush();
+		
+	}
+
+}
