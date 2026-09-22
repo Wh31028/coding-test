7212b593baa585a213fc57e97a9f7681456c4bde
Wh31028 <wh31028@naver.com>
2026-09-22 11:02:24 +0900

    solve : 타겟넘버, 힌트보면서 품.(dfs)

---
+++ programmers/DFS_BFS/타켓넘버.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int answer = 0;
+
+void dfs(vector<int>& numbers,int target, int index, int sum)
+{
+    if(index== numbers.size())
+    {
+        if(sum== target)
+        {
+            answer++;
+        }
+        return;
+    }
+    
+    dfs(numbers, target, index+1, sum+numbers[index]);
+    dfs(numbers, target, index+1, sum-numbers[index]);
+    
+    return;
+}
+
+int solution(vector<int> numbers, int target) {
+    answer = 0;
+    dfs(numbers,target,0,0);
+    
+    return answer;
+}
